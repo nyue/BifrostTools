ed667377831a80b6799d9823df68b89ff7f75dff
Nicholas Yue <yue.nicholas@gmail.com>
2015-01-14 10:26:31 -0800

    Added code to load and read Bifrost file

--- applications/bif2abc/main.cpp
+++ applications/bif2abc/main.cpp
@@ -37,8 +37,94 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
     return os;
 }
 
-bool process_bifrost_file(const std::string& bifrost_filename)
+int process_point_component(const Bifrost::API::Component& component,
+                            const std::string& position_channel_name,
+                            Imath::Box3f& bounds)
 {
+    int positionChannelIndex = findChannelIndexViaName(component,position_channel_name.c_str());
+    if (positionChannelIndex<0)
+        return 1;
+    const Bifrost::API::Channel& position_ch = component.channels()[positionChannelIndex];
+    if (!position_ch.valid())
+        return 1;
+    if ( position_ch.dataType() != Bifrost::API::FloatV3Type)
+        return 1;
+
+    Bifrost::API::Layout layout = component.layout();
+    size_t depthCount = layout.depthCount();
+    for ( size_t d=0; d<depthCount; d++ ) {
+        for ( size_t t=0; t<layout.tileCount(d); t++ ) {
+            Bifrost::API::TreeIndex tindex(t,d);
+            if ( !position_ch.elementCount( tindex ) ) {
+                // nothing there
+                continue;
+            }
+
+            const Bifrost::API::TileData<amino::Math::vec3f>& position_tile_data = position_ch.tileData<amino::Math::vec3f>( tindex );
+            // const Bifrost::API::TileData<amino::Math::vec3f>& velocity_tile_data = velocity_ch.tileData<amino::Math::vec3f>( tindex );
+            for (size_t i=0; i<position_tile_data.count(); i++ ) {
+                bounds.extendBy(Imath::V3f(position_tile_data[i][0],
+                                           position_tile_data[i][1],
+                                           position_tile_data[i][2]));
+            }
+        }
+    }
+    return 0;
+}
+
+bool process_bifrost_file(const std::string& bifrost_filename,
+                          const std::string& alembic_filename,
+                          const std::string& position_channel_name,
+                          const std::string& velocity_channel_name)
+{
+
+    Bifrost::API::String biffile = bifrost_filename.c_str();
+    Bifrost::API::ObjectModel om;
+    Bifrost::API::FileIO fileio = om.createFileIO( biffile );
+    const Bifrost::API::BIF::FileInfo& info = fileio.info();
+
+//     std::cout << boost::format("Version        : %1%") % info.version << std::endl;
+//     std::cout << boost::format("Frame          : %1%") % info.frame << std::endl;
+//     std::cout << boost::format("Channel count  : %1%") % info.channelCount << std::endl;
+//     std::cout << boost::format("Component name : %1%") % info.componentName.c_str() << std::endl;
+//     std::cout << boost::format("Component type : %1%") % info.componentType << std::endl;
+//     std::cout << boost::format("Object name    : %1%") % info.objectName.c_str() << std::endl;
+//     std::cout << boost::format("Layout name    : %1%") % info.layoutName.c_str() << std::endl;
+
+//     for (size_t channelIndex=0;channelIndex<info.channelCount;channelIndex++)
+//     {
+//         std::cout << std::endl;
+//         const Bifrost::API::BIF::FileInfo::ChannelInfo& channelInfo = fileio.channelInfo(channelIndex);
+//         std::cout << boost::format("        Channel name  : %1%") % channelInfo.name.c_str() << std::endl;
+//         std::cout << boost::format("        Data type     : %1%") % channelInfo.dataType << std::endl;
+//         std::cout << boost::format("        Max depth     : %1%") % channelInfo.maxDepth << std::endl;
+//         std::cout << boost::format("        Tile count    : %1%") % channelInfo.tileCount << std::endl;
+//         std::cout << boost::format("        Element count : %1%") % channelInfo.elementCount << std::endl;
+//     }
+
+    
+    // Need to load the entire file's content to process
+    Bifrost::API::StateServer ss = fileio.load( );
+    if (ss.valid())
+    {
+        size_t numComponents = ss.components().count();
+        for (size_t componentIndex=0;componentIndex<numComponents;componentIndex++)
+        {
+            Bifrost::API::Component component = ss.components()[componentIndex];
+            Bifrost::API::TypeID componentType = component.type();
+            if (componentType == Bifrost::API::PointComponentType)
+            {
+                Imath::Box3f bounds;
+                process_point_component(component, position_channel_name,bounds);
+            }
+        }
+    }
+    else
+    {
+        std::cerr << boost::format("Unable to load the content of the Bifrost file \"%1%\"") % bifrost_filename.c_str()
+                  << std::endl;
+        return false;
+    }
     return true;
 }
 
@@ -46,60 +132,35 @@ int main(int argc, char **argv)
 {
 
     try {
-        typedef std::vector<std::string> StringContainer;
         std::string position_channel_name("position");
         std::string velocity_channel_name("velocity");
-        StringContainer bifrost_filename_collection;
         std::string bifrost_filename;
+        std::string alembic_filename;
         float fps = 24.0f;
         po::options_description desc("Allowed options");
         desc.add_options()
-            ("version", "print version string")
-            ("help", "produce help message")
+            ("help", "Produce help message")
             ("fps", po::value<float>(&fps),
-                    "Frames per second to scale velocity when determining the velocity-attenuated bounding box. Defaults to 24.0")
-            ("input-files", po::value<StringContainer>(&bifrost_filename_collection),
-             "input files")
+             "Frames per second to scale velocity when determining the velocity-attenuated bounding box. Defaults to 24.0")
+            ("input-file,i", po::value<std::string>(&bifrost_filename),
+             "Bifrost file")
+            ("output-file,o", po::value<std::string>(&alembic_filename),
+             "Alembic file")
             ;
 
-        po::positional_options_description p;
-        p.add("input-files", -1);
-
         po::variables_map vm;
-        po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
+        po::store(po::parse_command_line(argc, argv, desc), vm);
         po::notify(vm);
 
-        if (vm.count("help")) {
+        if (vm.count("help") || bifrost_filename.empty() || alembic_filename.empty()) {
             std::cout << desc << "\n";
             return 1;
         }
-        if (vm.count("input-file"))
-        {
-            std::cout << "Input files are: "
-                      << vm["input-file"].as< StringContainer >() << "\n";
-            if (vm["input-file"].as< StringContainer >().size() == 1)
-            {
-                bifrost_filename = vm["input-file"].as< StringContainer >()[0];
-            }
-            else
-            {
-                std::cout << desc << "\n";
-                return 1;
-            }
-        }
-        std::cout << "fps = " << fps << std::endl;
-        StringContainer::const_iterator iter = bifrost_filename_collection.begin();
-        StringContainer::const_iterator eIter = bifrost_filename_collection.end();
-        for (;iter!=eIter;++iter)
-        {
-            std::cout << "bifrost file to process " << iter->c_str() << std::endl;
-            process_bifrost_file(*iter);
-//            process_bifrost_file(bifrost_filename,
-//                                 position_channel_name,
-//                                 velocity_channel_name,
-//                                 bbox_type,
-//                                 bbox_type==BBOX::PointsWithVelocity?&fps:0);
-        }
+
+        process_bifrost_file(bifrost_filename,
+                             alembic_filename,
+                             position_channel_name,
+                             velocity_channel_name);
     }
     catch(std::exception& e) {
         std::cerr << "error: " << e.what() << "\n";
