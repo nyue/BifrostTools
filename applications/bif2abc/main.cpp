ba415fe4a21902ebce4de3930836629b10ed459a
Nicholas Yue <yue.nicholas@gmail.com>
2015-01-14 23:21:00 -0800

    Add velocity handling for Houdini alembic convention

--- applications/bif2abc/main.cpp
+++ applications/bif2abc/main.cpp
@@ -47,10 +47,12 @@ addXform(Alembic::Abc::OObject parent, std::string name)
 
 bool process_point_component(const Bifrost::API::Component& component,
                             const std::string& position_channel_name,
+                            const std::string& velocity_channel_name,
                             Imath::Box3f& bounds,
                             uint32_t tsidx,
                             Alembic::AbcGeom::OXform& xform)
 {
+    // Position channel
     int positionChannelIndex = findChannelIndexViaName(component,position_channel_name.c_str());
     if (positionChannelIndex<0)
         return false;
@@ -60,16 +62,35 @@ bool process_point_component(const Bifrost::API::Component& component,
     if ( position_ch.dataType() != Bifrost::API::FloatV3Type)
         return false;
 
+    // Velocity channel
+    int velocityChannelIndex = findChannelIndexViaName(component,velocity_channel_name.c_str());
+    if (velocityChannelIndex<0)
+        return false;
+    const Bifrost::API::Channel& velocity_ch = component.channels()[velocityChannelIndex];
+    if (!velocity_ch.valid())
+        return false;
+    if ( velocity_ch.dataType() != Bifrost::API::FloatV3Type)
+        return false;
+
     // Create the OPoints object
     Alembic::AbcGeom::OPoints partsOut(xform,component.name().c_str(),tsidx);
     Alembic::AbcGeom::OPointsSchema &pSchema = partsOut.getSchema();
 
+    Alembic::AbcGeom::MetaData mdata;
+    SetGeometryScope( mdata, Alembic::AbcGeom::kVaryingScope );
+    Alembic::AbcGeom::OV3fArrayProperty velOut( pSchema, ".velocities", mdata, tsidx );
 
+    std::vector< Alembic::Abc::V3f > positions;
+    std::vector< Alembic::Abc::V3f > velocities;
+    std::vector< Alembic::Util::uint64_t > ids;
 
+    // NOTE : Other than position, velocity and id, all the other information
+    //        are expected to be store as arbGeomParam
 
-
+    // Data accumulation
     Bifrost::API::Layout layout = component.layout();
     size_t depthCount = layout.depthCount();
+    Alembic::Util::uint64_t currentId = 0;
     for ( size_t d=0; d<depthCount; d++ ) {
         for ( size_t t=0; t<layout.tileCount(d); t++ ) {
             Bifrost::API::TreeIndex tindex(t,d);
@@ -79,14 +100,39 @@ bool process_point_component(const Bifrost::API::Component& component,
             }
 
             const Bifrost::API::TileData<amino::Math::vec3f>& position_tile_data = position_ch.tileData<amino::Math::vec3f>( tindex );
-            // const Bifrost::API::TileData<amino::Math::vec3f>& velocity_tile_data = velocity_ch.tileData<amino::Math::vec3f>( tindex );
-            for (size_t i=0; i<position_tile_data.count(); i++ ) {
-                bounds.extendBy(Imath::V3f(position_tile_data[i][0],
-                                           position_tile_data[i][1],
-                                           position_tile_data[i][2]));
+            const Bifrost::API::TileData<amino::Math::vec3f>& velocity_tile_data = velocity_ch.tileData<amino::Math::vec3f>( tindex );
+            if (position_tile_data.count() != velocity_tile_data.count())
+            {
+                std::cerr << boost::format("Point position and velocity tile data count mismatch position[%1%] velocity[%2%]") % position_tile_data.count() % velocity_tile_data.count()<< std::endl;
+                return false;
+            }
+            for (size_t i=0; i<position_tile_data.count(); i++ )
+            {
+                Imath::V3f point_position(position_tile_data[i][0],
+                        position_tile_data[i][1],
+                        position_tile_data[i][2]);
+                positions.push_back(point_position);
+                bounds.extendBy(point_position);
+                ids.push_back(currentId);
+                currentId++;
+            }
+
+            for (size_t i=0; i<velocity_tile_data.count(); i++ )
+            {
+                velocities.push_back(Imath::V3f (velocity_tile_data[i][0],
+                        velocity_tile_data[i][1],
+                        velocity_tile_data[i][2]));
             }
         }
     }
+
+    // Update Alembic storage
+    Alembic::AbcGeom::V3fArraySample position_data ( positions );
+    Alembic::AbcGeom::UInt64ArraySample id_data ( ids );
+    Alembic::AbcGeom::OPointsSchema::Sample psamp(position_data,
+                id_data);
+    pSchema.set( psamp );
+    velOut.set( Alembic::AbcGeom::V3fArraySample( velocities ) );
     return true;
 }
 
@@ -156,7 +202,7 @@ bool process_bifrost_file(const std::string& bifrost_filename,
                 if (componentType == Bifrost::API::PointComponentType)
                 {
                     Imath::Box3f bounds;
-                    process_point_component(component, position_channel_name,bounds,tsidx,xform);
+                    process_point_component(component, position_channel_name,velocity_channel_name,bounds,tsidx,xform);
                 }
             }
         }
