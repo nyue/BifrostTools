31bbc88c16341c6e6fb05e0abcb81f5842cfedfb
Nicholas Yue <yue.nicholas@gmail.com>
2015-10-18 14:59:16 -0700

    Remove unused include Added edit indentation hints

--- houdini/bif2bgeo/bif2bgeo.cpp
+++ houdini/bif2bgeo/bif2bgeo.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <boost/program_options.hpp>
 #include <boost/format.hpp>
--- houdini/converter/gbifrost.cpp
+++ houdini/converter/gbifrost.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <boost/format.hpp>
 
@@ -24,3 +23,16 @@ int main(int argc, char** argv)
 	}
     return 0;
 }
+// == Emacs ================
+// -------------------------
+// Local variables:
+// tab-width: 4
+// indent-tabs-mode: t
+// c-basic-offset: 4
+// end:
+//
+// == vi ===================
+// -------------------------
+// Format block
+// ex:ts=4:sw=4:expandtab
+// -------------------------
