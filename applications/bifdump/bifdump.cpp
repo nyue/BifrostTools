5d6b8a0d158018778267f7ff93175871900cdab9
Nicholas Yue <yue.nicholas@gmail.com>
2017-03-26 12:31:36 -0700

    Factor out data dump routine into a separate function

--- applications/bifdump/bifdump.cpp
+++ applications/bifdump/bifdump.cpp
@@ -1,6 +1,66 @@
 #include <BifrostHeaders.h>
 #include <boost/format.hpp>
 
+void perform_dump(const Bifrost::API::Layout&  layout,
+                  const Bifrost::API::Channel& ch)
+{
+    // iterate over the tile tree at each level
+    Bifrost::API::DataType channelDataType = ch.dataType();
+    size_t depthCount = layout.depthCount();
+    for ( size_t d=0; d<depthCount; d++ ) {
+        for ( size_t t=0; t<layout.tileCount(d); t++ ) {
+            Bifrost::API::TreeIndex tindex(t,d);
+            if ( !ch.elementCount( tindex ) ) {
+                // nothing there
+                continue;
+            }
+            std::cout << "tile:" << t << " depth:" << d << std::endl;
+            switch (channelDataType)
+            {
+            case Bifrost::API::FloatType :
+                {
+                    const Bifrost::API::TileData<float>& f1 = ch.tileData<float>( tindex );
+                    for (size_t i=0; i<f1.count(); i++ ) {
+                        const float& val = f1[i];
+                        std::cout << "\t" << f1[i] << std::endl;
+                    }
+
+                    std::cout << std::endl;
+                }
+                break;
+            case Bifrost::API::FloatV2Type :
+                break;
+            case Bifrost::API::FloatV3Type :
+                {
+                    const Bifrost::API::TileData<amino::Math::vec3f>& f3 = ch.tileData<amino::Math::vec3f>( tindex );
+                    for (size_t i=0; i<f3.count(); i++ ) {
+                        const amino::Math::vec3f& val = f3[i];
+                        std::cout << "\t" << val[0] << " " << val[1] << " " << val[2] << std::endl;
+                    }
+
+                    std::cout << std::endl;
+                }
+                break;
+            case Bifrost::API::Int32Type :
+                break;
+            case Bifrost::API::Int64Type :
+                break;
+            case Bifrost::API::UInt32Type :
+                break;
+            case Bifrost::API::UInt64Type :
+                break;
+            case Bifrost::API::Int32V2Type :
+                break;
+            case Bifrost::API::Int32V3Type :
+                break;
+            default:
+                std::cerr << "Unknown channel type encountered" << std::endl;
+                break;
+            }
+        }
+    }
+}
+
 int main(int argc, char **argv)
 {
     if (argc != 2)
@@ -24,6 +84,7 @@ int main(int argc, char **argv)
     {
         Bifrost::API::Component component = ss.components()[i];
         Bifrost::API::TypeID componentType = component.type();
+        Bifrost::API::Layout layout = component.layout();
         if (componentType == Bifrost::API::PointComponentType)
         {
             std::cout << "Point component : "
@@ -40,65 +101,7 @@ int main(int argc, char **argv)
                 Bifrost::API::DataType channelDataType = ch.dataType();
                 std::cout << boost::format("\tChannel[%1%] of type %2% : %3% has %4% particles")
                 % channelIndex % channelDataType % channelName.c_str() % channelCount << std::endl;
-                bool doDump = true;
-                if (doDump)
-                {
-                    // iterate over the tile tree at each level
-                    Bifrost::API::Layout layout = component.layout();
-                    size_t depthCount = layout.depthCount();
-                    for ( size_t d=0; d<depthCount; d++ ) {
-                        for ( size_t t=0; t<layout.tileCount(d); t++ ) {
-                            Bifrost::API::TreeIndex tindex(t,d);
-                            if ( !ch.elementCount( tindex ) ) {
-                                // nothing there
-                                continue;
-                            }
-                            std::cout << "tile:" << t << " depth:" << d << std::endl;
-                            switch (channelDataType)
-                            {
-                            case Bifrost::API::FloatType :
-                            {
-                                const Bifrost::API::TileData<float>& f1 = ch.tileData<float>( tindex );
-                                for (size_t i=0; i<f1.count(); i++ ) {
-                                    const float& val = f1[i];
-                                    std::cout << "\t" << f1[i] << std::endl;
-                                }
-
-                                std::cout << std::endl;
-                            }
-                            break;
-                            case Bifrost::API::FloatV2Type :
-                                break;
-                            case Bifrost::API::FloatV3Type :
-                            {
-                                const Bifrost::API::TileData<amino::Math::vec3f>& f3 = ch.tileData<amino::Math::vec3f>( tindex );
-                                for (size_t i=0; i<f3.count(); i++ ) {
-                                    const amino::Math::vec3f& val = f3[i];
-                                    std::cout << "\t" << val[0] << " " << val[1] << " " << val[2] << std::endl;
-                                }
-
-                                std::cout << std::endl;
-                            }
-                            break;
-                            case Bifrost::API::Int32Type :
-                                break;
-                            case Bifrost::API::Int64Type :
-                                break;
-                            case Bifrost::API::UInt32Type :
-                                break;
-                            case Bifrost::API::UInt64Type :
-                                break;
-                            case Bifrost::API::Int32V2Type :
-                                break;
-                            case Bifrost::API::Int32V3Type :
-                                break;
-                            default:
-                                std::cerr << "Unknown channel type encountered" << std::endl;
-                                break;
-                            }
-                        }
-                    }
-                }
+                perform_dump(layout,ch);
             }
         }
         else if (componentType == Bifrost::API::VoxelComponentType)
@@ -110,10 +113,12 @@ int main(int argc, char **argv)
             size_t channelCount = channels.count();
             for (size_t channelIndex=0;channelIndex<channelCount;channelIndex++)
             {
-                Bifrost::API::String channelName = Bifrost::API::Base(channels[channelIndex]).name();
-                std::cout << "\tChannel : "
-                        << channelName.c_str()
-                        << std::endl;
+                const Bifrost::API::Channel& ch = channels[channelIndex];
+                Bifrost::API::String channelName = ch.name();
+                Bifrost::API::DataType channelDataType = ch.dataType();
+                std::cout << boost::format("\tChannel[%1%] of type %2% : %3% has %4% voxels")
+                % channelIndex % channelDataType % channelName.c_str() % channelCount << std::endl;
+                perform_dump(layout,ch);
             }
         }
     }
